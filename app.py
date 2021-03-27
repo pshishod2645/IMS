ee73f5641078839d449dd86c8f88f2042c5f1ae8
Prashant Shishodia <prashantshishodia.dakshana17@gmail.com>
2021-03-27 17:28:28 +0530

    Initialized Flask app

---
+++ app.py
@@ -0,0 +1,64 @@
+import os
+from flask import Flask 
+from flask import render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+from models import * 
+from flask_admin.contrib.sqla import ModelView
+
+#Configurations
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+admin = Admin(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+@login_manager.user_loader
+def load_user(user): 
+    return user
+
+#Configuration ends here 
+
+#Routes 
+@app.route('/')
+def home(): 
+    return render_template('home.j2', title = 'Home')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login(): 
+    if current_user.is_authenticated: 
+        return redirect('/')
+
+    if request.method == 'POST' : 
+        username, password = request.form.get('username'), request.form.get('password')
+        user = User.query.filter((User.email == username or User.username == username) and User.password == password).first()
+        # print(user)
+        if user : 
+            login_user(user)
+            return redirect('/')
+    return render_template('login.j2')
+
+@app.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated: 
+        return redirect('/')
+
+    if request.method == 'POST':        # only for students
+        print(requests.form)
+    return render_template('signup.j2')
+
+def logout(): 
+    logout_user()
+    return redirect('/')
+
+
+if __name__ == '__main__' : 
+    app.secret_key = os.urandom(12)
+    app.run(debug = True)
